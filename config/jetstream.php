a13e9eb2586ace9244b4bdcac0673be57d138956
Refringe <me@refringe.com>
2024-05-14 00:09:13 -0400

    Configures Jetstream, Fortify, and installs Pulse

--- config/jetstream.php
+++ config/jetstream.php
@@ -58,10 +58,9 @@ return [
     */
 
     'features' => [
-        // Features::termsAndPrivacyPolicy(),
-        // Features::profilePhotos(),
-        // Features::api(),
-        // Features::teams(['invitations' => true]),
+        Features::termsAndPrivacyPolicy(),
+        Features::profilePhotos(),
+        Features::api(),
         Features::accountDeletion(),
     ],
 
