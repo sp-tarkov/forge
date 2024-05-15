5c295ffafa5b9ee9d69c9bf696313433b763a5d1
Refringe <me@refringe.com>
2024-05-14 22:15:20 -0400

    Adds Prettier Formatting

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
