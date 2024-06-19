bf29b348a5f1f1d591f73761cc567a6f6c7eab47
Refringe <me@refringe.com>
2024-06-19 14:03:36 -0400

    Moves Livewire Assets to R2

--- app/Console/Commands/UploadAssets.php
+++ app/Console/Commands/UploadAssets.php
@@ -10,7 +10,7 @@ class UploadAssets extends Command
 {
     protected $signature = 'app:upload-assets';
 
-    protected $description = 'Uploads the Vite build assets to Cloudflare R2';
+    protected $description = 'Uploads assets to Cloudflare R2';
 
     /**
      * This command uploads the Vite build assets to Cloudflare R2. Typically, this will be run after the assets have
@@ -18,15 +18,35 @@ class UploadAssets extends Command
      */
     public function handle(): void
     {
-        $this->info('Publishing assets...');
+        $this->publishBuildAssets();
+        $this->publishVendorAssets();
+    }
 
-        $buildFiles = File::allFiles(public_path('/build'));
-        foreach ($buildFiles as $asset) {
+    protected function publishBuildAssets(): void
+    {
+        $this->info('Publishing build assets...');
+
+        $assets = File::allFiles(public_path('/build'));
+        foreach ($assets as $asset) {
             $buildDir = 'build/'.$asset->getRelativePathname();
             $this->info('Uploading asset to: '.$buildDir);
             Storage::disk('r2')->put($buildDir, $asset->getContents());
         }
 
-        $this->info('Assets published successfully');
+        $this->info('Build assets published successfully');
+    }
+
+    protected function publishVendorAssets(): void
+    {
+        $this->info('Publishing vendor assets...');
+
+        $assets = File::allFiles(public_path('/vendor'));
+        foreach ($assets as $asset) {
+            $buildDir = 'vendor/'.$asset->getRelativePathname();
+            $this->info('Uploading asset to: '.$buildDir);
+            Storage::disk('r2')->put($buildDir, $asset->getContents());
+        }
+
+        $this->info('Build assets published successfully');
     }
 }
