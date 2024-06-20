e7ab5bfb340f1232ecd6871e1826607935a3efdd
Refringe <me@refringe.com>
2024-06-20 11:13:25 -0400

    Removes Banhammer Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Mchev\Banhammer\Middleware\IPBanned;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(IPBanned::class);
+        //$middleware->append(IPBanned::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
