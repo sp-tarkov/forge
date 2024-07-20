11453db596da60e3bdab8d4e6f23bf5cbe306db7
Refringe <me@refringe.com>
2024-07-20 19:52:36 -0400

    Model Comments, Yay!

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+
     <div class="sm:-mt-12 dark:bg-gray-800 dark:text-gray-100">
         <div>
             <img class="h-32 w-full object-cover lg:h-48" src="https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="">
@@ -30,6 +31,5 @@
             </div>
         </div>
     </div>
-
 
 </x-app-layout>
