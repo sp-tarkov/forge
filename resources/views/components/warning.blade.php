eb9a656c6971ec9f21bc0aed137169a110eb5fef
Refringe <me@refringe.com>
2024-09-17 16:47:43 -0400

    RAINBOWS
    Need I say more?

--- resources/views/components/warning.blade.php
+++ resources/views/components/warning.blade.php
@@ -1,3 +1,4 @@
-<div class="items-center justify-between gap-x-6 text-gray-200 bg-gray-900 dark:text-gray-900 dark:bg-gray-100 px-6 py-2.5 sm:pr-3.5 lg:pl-8">
-    <p class="text-center text-sm leading-6">Notice: The Forge is currently under <em class="font-bold">heavy</em> construction. Expect nothing to work. Data is reset every hour.</p>
+<div class="relative items-center justify-between gap-x-6 text-gray-900 px-6 py-2.5 sm:pr-3.5 lg:pl-8">
+    <div class="rainbow z-0"></div>
+    <p class="relative z-10 text-center text-sm leading-6 font-bold" style="text-shadow: 0 0 15px white;">{!! __('The Forge is currently under <em>heavy</em> construction. Expect nothing to work. Data is reset every hour.') !!}</p>
 </div>
