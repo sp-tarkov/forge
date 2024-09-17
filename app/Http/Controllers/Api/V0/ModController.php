297937a2ea1e7f74c3bd3a3e21b5eabc13289bf7
Refringe <me@refringe.com>
2024-09-17 01:41:00 -0400

    Scribe API Documentation
    Installs Scribe for (mostly) automatic API documentation. Does a pretty good job and it's pretty painless to set-up. I'll have to configure it to work in the CI and to ensure that the static files are being pushed to production as they should be, but this is a good start.

--- app/Http/Controllers/Api/V0/ModController.php
+++ app/Http/Controllers/Api/V0/ModController.php
@@ -3,43 +3,58 @@
 namespace App\Http\Controllers\Api\V0;
 
 use App\Http\Filters\V1\ModFilter;
-use App\Http\Requests\Api\V0\StoreModRequest;
-use App\Http\Requests\Api\V0\UpdateModRequest;
 use App\Http\Resources\Api\V0\ModResource;
 use App\Models\Mod;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Knuckles\Scribe\Attributes\QueryParam;
+use Knuckles\Scribe\Attributes\UrlParam;
 
 class ModController extends ApiController
 {
     /**
-     * Display a listing of the resource.
+     * Get Mods
+     *
+     * List, filter, and sort basic information about mods.
+     *
+     * @group Mods
      */
+    #[QueryParam('include', 'string', 'The relationships to include within the `includes` key. By default no relationships are automatically included.', required: false, example: 'users,versions,license')]
+    #[QueryParam('filter[id]', 'string', 'Filter by the `id`. Select multiple by separating the IDs with a comma.', required: false, example: '5,10,15')]
+    #[QueryParam('filter[hub_id]', 'string', 'Filter by the `hub_id` attribute. Select multiple by separating the IDs with a comma.', required: false, example: '20')]
+    #[QueryParam('filter[name]', 'string', 'Filter by the `name` attribute. Use `*` as the wildcard character.', required: false, example: '*SAIN*')]
+    #[QueryParam('filter[slug]', 'string', 'Filter by the `slug` attribute. Use `*` as the wildcard character.', required: false, example: '*raid-times')]
+    #[QueryParam('filter[teaser]', 'string', 'Filter by the `teaser` attribute. Use `*` as the wildcard character.', required: false, example: '*weighted*random*times*')]
+    #[QueryParam('filter[source_code_link]', 'string', 'Filter by the `source_code_link` attribute. Use `*` as the wildcard character.', required: false, example: '*https*.net*')]
+    #[QueryParam('filter[featured]', 'boolean', 'Filter by the `featured` attribute. All "truthy" or "falsy" values are supported.', required: false, example: 'true')]
+    #[QueryParam('filter[contains_ads]', 'boolean', 'Filter by the `contains_ads` attribute. All "truthy" or "falsy" values are supported.', required: false, example: 'true')]
+    #[QueryParam('filter[contains_ai_content]', 'boolean', 'Filter by the `contains_ai_content` attribute. All "truthy" or "falsy" values are supported.', required: false, example: 'true')]
+    #[QueryParam('filter[created_at]', 'string', 'Filter by the `created_at` attribute. Ranges are possible by separating the dates with a comma.', required: false, example: '2023-12-31,2024-12-31')]
+    #[QueryParam('filter[updated_at]', 'string', 'Filter by the `updated_at` attribute. Ranges are possible by separating the dates with a comma.', required: false, example: '2023-12-31,2024-12-31')]
+    #[QueryParam('filter[published_at]', 'string', 'Filter by the `published_at` attribute. Ranges are possible by seperating the dates with a comma.', required: false, example: '2023-12-31,2024-12-31')]
+    #[QueryParam('sort', 'string', 'Sort the results by a comma seperated list of attributes. The default sort direction is ASC, append the attribute name with a minus to sort DESC.', required: false, example: '-featured,name')]
     public function index(ModFilter $filters): AnonymousResourceCollection
     {
         return ModResource::collection(Mod::filter($filters)->paginate());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreModRequest $request): void {}
+    //public function store(StoreModRequest $request): void {}
 
     /**
-     * Display the specified resource.
+     * Get Mod
+     *
+     * Display more detailed information about a specific mod.
+     *
+     * @group Mods
      */
+    #[UrlParam('id', 'integer', 'The ID of the mod.', required: true, example: 558)]
+    #[QueryParam('include', 'string', 'The relationships to include within the `includes` key. By default no relationships are automatically included.', required: false, example: 'users,versions,license')]
     public function show(Mod $mod): JsonResource
     {
         return new ModResource($mod);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateModRequest $request, Mod $mod): void {}
+    //public function update(UpdateModRequest $request, Mod $mod): void {}
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Mod $mod): void {}
+    //public function destroy(Mod $mod): void {}
 }
--- app/Http/Controllers/Api/V0/UsersController.php
+++ app/Http/Controllers/Api/V0/UsersController.php
@@ -3,43 +3,48 @@
 namespace App\Http\Controllers\Api\V0;
 
 use App\Http\Filters\V1\UserFilter;
-use App\Http\Requests\Api\V0\StoreUserRequest;
-use App\Http\Requests\Api\V0\UpdateUserRequest;
 use App\Http\Resources\Api\V0\UserResource;
 use App\Models\User;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Knuckles\Scribe\Attributes\QueryParam;
 
 class UsersController extends ApiController
 {
     /**
-     * Display a listing of the resource.
+     * Get Users
+     *
+     * List, filter, and sort basic information about users.
+     *
+     * @group Users
      */
+    #[QueryParam('include', 'string', 'The relationships to include within the `includes` key. By default no relationships are automatically included.', required: false, example: 'user_role')]
+    #[QueryParam('filter[id]', 'string', 'Filter by the `id`. Select multiple by separating the IDs with a comma.', required: false, example: '5,10,15')]
+    #[QueryParam('filter[name]', 'string', 'Filter by the `name` attribute. Use `*` as the wildcard character.', required: false, example: '*fringe')]
+    #[QueryParam('filter[created_at]', 'string', 'Filter by the `created_at` attribute. Ranges are possible by separating the dates with a comma.', required: false, example: '2023-12-31,2024-12-31')]
+    #[QueryParam('filter[updated_at]', 'string', 'Filter by the `updated_at` attribute. Ranges are possible by separating the dates with a comma.', required: false, example: '2023-12-31,2024-12-31')]
+    #[QueryParam('sort', 'string', 'Sort the results by a comma seperated list of attributes. The default sort direction is ASC, append the attribute name with a minus to sort DESC.', required: false, example: 'created_at,-name')]
     public function index(UserFilter $filters): AnonymousResourceCollection
     {
         return UserResource::collection(User::filter($filters)->paginate());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreUserRequest $request): void {}
+    //public function store(StoreUserRequest $request): void {}
 
     /**
-     * Display the specified resource.
+     * Get User
+     *
+     * Display more detailed information about a specific user.
+     *
+     * @group Users
      */
+    #[QueryParam('include', 'string', 'The relationships to include within the `includes` key. By default no relationships are automatically included.', required: false, example: 'user_role')]
     public function show(User $user): JsonResource
     {
         return new UserResource($user);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateUserRequest $request, User $user): void {}
+    //public function update(UpdateUserRequest $request, User $user): void {}
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user): void {}
+    //public function destroy(User $user): void {}
 }
