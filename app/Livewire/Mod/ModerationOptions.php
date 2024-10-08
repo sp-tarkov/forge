8e8a654133b0efd0fd5e069eb1254f08920858a0
waffle.lord <waffle.lord@hotmail.com>
2024-10-08 13:15:12 -0400

    add livewire component

---
+++ app/Livewire/Mod/ModerationOptions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Mod;
+
+use Livewire\Component;
+
+class ModerationOptions extends Component
+{
+    public function render()
+    {
+        return view('livewire..mod.moderation-options');
+    }
+}
