36e1ebeafc2535dfe205fee2bbd017fde2cd5474
Refringe <me@refringe.com>
2025-03-09 23:07:21 -0400

    Mod filter menus now close after being used

--- resources/views/components/filter-menu-item.blade.php
+++ resources/views/components/filter-menu-item.blade.php
@@ -1,7 +1,7 @@
 @props(['filterName', 'filter', 'currentFilter'])
 
 <a href="#{{ $filter }}"
-   @click.prevent="$wire.set('{{ $filterName }}', '{{ $filter }}')"
+   @click.prevent="$wire.set('{{ $filterName }}', '{{ $filter }}'); isResultsPerPageOpen = false; isSortOpen = false"
    class="flex items-center gap-2 bg-slate-100 px-4 py-2 text-sm hover:bg-slate-800/5 focus-visible:bg-slate-800/10 focus-visible:text-black focus-visible:outline-hidden dark:bg-slate-800 dark:hover:bg-slate-100/5 dark:focus-visible:bg-slate-100/10 dark:focus-visible:text-white {{ strval($filter) === strval($currentFilter) ? "font-bold text-cyan-500 dark:text-cyan-500 hover:text-cyan-400 dark:hover:text-cyan-400" : "text-slate-700 dark:text-slate-300 hover:text-black dark:hover:text-white" }}"
    role="menuitem" tabindex="-1">
     {{ $slot }}
