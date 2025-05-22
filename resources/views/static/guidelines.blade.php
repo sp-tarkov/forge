48408cf23ff58e8c77b784c3a8354b218a251f21
CWX <cwxdev@outlook.com>
2025-05-22 22:16:42 +0100

    start of guidelines and TOS

--- resources/views/static/guidelines.blade.php
+++ resources/views/static/guidelines.blade.php
@@ -1,22 +1,81 @@
 <x-app-layout>
     <x-slot name="title">
-        {{ __('Releases') }}
+        {{ __('Community Guidelines') }}
     </x-slot>
     <x-slot name="description">
-        {{ __('Releases page for SPT') }}
+        {{ __('Community Guidelines for SPT') }}
     </x-slot>
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-100 leading-tight">
-            {{ __('Releases') }}
+            {{ __('Community Guidelines') }}
         </h2>
     </x-slot>
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-900 overflow-hidden shadow-xl sm:rounded-lg">
-            <div class="px-4 py-8 sm:px-6 lg:px-8 bg-white dark:bg-gray-900 overflow-hidden shadow-xl dark:shadow-gray-900 rounded-none sm:rounded-lg">
+            <div
+                class="px-4 py-8 sm:px-6 lg:px-8 bg-white dark:bg-gray-900 overflow-hidden shadow-xl dark:shadow-gray-900 rounded-none sm:rounded-lg">
+                <p class="font-semibold text-xl mb-1">Rules:</p>
+                <ul class="list-disc list-inside mb-4">
+                    <li>Don't break Discord's Terms of Use.</li>
+                    <li>Don't ping or DM staff or developers with game support questions.</li>
+                    <li>Don't promote other platforms and Discord servers.</li>
+                    <li>Don't post, share or request Escape From Tarkov client files.</li>
+                    <li>Don't show or discuss Escape From Tarkov cheats or exploits.</li>
+                    <li>Don't steal other people's work.</li>
+                    <li>Only post support related questions in the Discord Support Channel</li>
+                    <li>Don't ask for money for services you provide (i.e. creating / distributing mods, selling in-game
+                        roubles) or promote such acts or platforms.
+                    </li>
+                    <li>Don't discuss politics.</li>
+                    <li>Only use English for communicating on the website.</li>
+                </ul>
 
+                <p class="font-semibold text-xl mb-1">Things to know:</p>
 
+                <p class="font-semibold">External sources:</p>
+                <p>Any site/platform that redistributes our work or content in any shape or form that isn't acquired
+                    from:</p>
+                <ul class="list-disc list-inside mb-4">
+                    <li>*.sp-tarkov.com</li>
+                    <li>
+                        <a href="#" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white" target="_blank">Official discord</a>
+                    </li>
+                    <li>One of our affiliated communities</li>
+                </ul>
+                <p>Is not endorsed or affiliated with/by us.</p>
+                <p class="mb-4">
+                    Only <a href="https://www.reddit.com/r/sptarkov" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white" target="_blank">https://www.reddit.com/r/sptarkov</a>
+                    is an external community which we also manage.
+                </p>
+
+                <p class="font-semibold">Affiliated communities:</p>
+                <ul class="list-disc list-inside mb-4">
+                    <li>
+                        <a href="https://sns.oddba.cn/" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white" target="_blank">
+                            Chinese SPTarkov community
+                        </a>
+                        (not managed by us!)
+                    </li>
+                    <li>
+                        <a href="https://discord.gg/c4RuJNs" target="_blank" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white">
+                            Anomaly Discord
+                        </a>
+                    </li>
+                </ul>
+
+                <a href="https://hub.sp-tarkov.com/advertising-disclaimer/" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white">Find more about unaffiliated links</a>
+
+                <p class="font-semibold mt-4">You also need to take care of the following rules:</p>
+                <ul class="list-disc list-inside">
+                    <li><a href="https://hub.sp-tarkov.com/file-submission-guidelines/" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white">File submission guidelines</a></li>
+                    <li><a href="https://hub.sp-tarkov.com/terms-of-service/" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white">Terms of service</a></li>
+                    <li><a href="https://hub.sp-tarkov.com/forum-and-commenting-guidelines/" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white">Forum and commenting guildelines</a></li>
+                    <li><a href="https://hub.sp-tarkov.com/reporting-guidelines/" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white">Reporting guidelines</a></li>
+                    <li><a href="https://discord.com/terms" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white" target="_blank">Discord Terms of Use</a></li>
+                    <li><a href="https://discord.com/guidelines" class="underline text-gray-800 hover:text-black dark:text-gray-200 dark:hover:text-white" target="_blank">Discord Community Guidelines</a></li>
+                </ul>
             </div>
         </div>
     </div>
--- resources/views/static/tos.blade.php
+++ resources/views/static/tos.blade.php
@@ -1,22 +1,148 @@
 <x-app-layout>
     <x-slot name="title">
-        {{ __('Releases') }}
+        {{ __('Terms of Service') }}
     </x-slot>
     <x-slot name="description">
-        {{ __('Releases page for SPT') }}
+        {{ __('Terms of Service for SPT') }}
     </x-slot>
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-100 leading-tight">
-            {{ __('Releases') }}
+            {{ __('Terms of Service') }}
         </h2>
     </x-slot>
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-900 overflow-hidden shadow-xl sm:rounded-lg">
-            <div class="px-4 py-8 sm:px-6 lg:px-8 bg-white dark:bg-gray-900 overflow-hidden shadow-xl dark:shadow-gray-900 rounded-none sm:rounded-lg">
+            <div
+                class="px-4 py-8 sm:px-6 lg:px-8 bg-white dark:bg-gray-900 overflow-hidden shadow-xl dark:shadow-gray-900 rounded-none sm:rounded-lg">
+                <p class="font-semibold text-xl">Terms of service related to sp-tarkov.com:</p>
+                <p class="pb-4">
+                    SPT strives to provide its users with services and information that facilitate the discussion and
+                    distribution of user-submitted Escape From Tarkov - SPT related game modifications ("mods"). To that
+                    end, this document establishes various acknowledgements, entitlements, and restrictions in regards
+                    to the usage of SPT websites, content, systems, and software. SPT reserves the right to discontinue
+                    providing any or all of these services, as well as cease any form of communications, to any user or
+                    organisation and for any reason and at any time.
+                </p>
 
+                <p class="font-semibold text-xl">General Usage of Websites and Services:</p>
+                <p class="pb-4">
+                    By accessing SPT services, you hereby represent that you are aged 16 or older.
+                    You will use SPT services reasonably and as intended. You may not display, duplicate, dismantle,
+                    reverse engineer, circumvent, or repurpose any property or service owned or operated by SPT without
+                    consent. You will not intentionally harm, disparage, or in any way disrupt the operation of SPT
+                    websites, systems, property, or staff. You view and use all content at your own risk. We do not
+                    warrant the accuracy, completeness or usefulness of any content presented. User-submitted
+                    content expresses the views of the author, not necessarily the views of any staff at SPT.
+                    In the interest of upholding our community standards when it comes to etiquette, we ask you to
+                    abstain from interacting with user-submitted content that you do not find to your liking,
+                    rather than engaging in active disparagement of content you disapprove of.
+                </p>
 
+                <p class="font-semibold text-xl">Anti-Piracy Policy:</p>
+                <p class="pb-4">
+                    We have a strict anti-piracy policy and will take proactive action against anyone whose on-site
+                    activity gives us reasonable grounds to believe they are using pirated content such as, but not
+                    limited to, games and software. Discussing, promoting or distributing such content on our sites will
+                    be met with administrative action.
+                </p>
+
+                <p class="font-semibold text-xl">Fair Use Policy:</p>
+                <p class="pb-4">
+                    Attempting to download files or otherwise record data offered through our services (including but
+                    not limited to the SPT website) in a fashion that drastically exceeds the expected average, through
+                    the use of software automation or otherwise, is prohibited without expressed permission. Users found
+                    in violation of this policy will have their account suspended.
+                </p>
+
+                <p class="font-semibold text-xl">User-submitted Content:</p>
+                <p class="pb-4">
+                    User-submitted content includes all data submitted to our services by a SPT user. SPT does not claim
+                    any ownership of your content. By submitting content to our services, you are granting an infinite,
+                    non-exclusive, worldwide, royalty-free license for SPT to store, distribute, copy or reproduce,
+                    edit, translate, reformat, publicly display, or perform the submitted content, at our discretion.
+                </p>
+                <p class="pb-4">
+                    All user-submitted content is provided for personal use. You are not entitled to redistribute,
+                    repackage, sell, or otherwise distribute content without express permission from the associated
+                    content owner(s), and/or other invested parties, when applicable. For more information, refer to any
+                    applicable documentation included or associated with the file.
+                </p>
+                <p class="pb-4">
+                    By submitting content to our services, you understand that your SPT account and associated content
+                    will be held to the standards which are described by our Guidelines and may be subject to review
+                    which can result in administrative actions. Additionally, you understand that we reserve the right
+                    to remove any content with or without reason at our discretion.
+                </p>
+                <p class="pb-4">
+                    Given the volume of user-submitted content on our services, we rely on community reports and
+                    reactive moderation when it comes to upholding our various rules and guidelines. The mere existence
+                    of a file violating our rules on our services does not imply our approval of, or indifference to
+                    said file. As such, user-submitted content which is found to be in violation of our standards is not
+                    excusable based on the existence of other, similar submissions.
+                </p>
+
+                <p class="font-semibold text-xl">Prohibited and Illicit Content:</p>
+                <p class="pb-4">
+                    Content deemed illegal by EU law, including but not limited to pornographic depictions of sexual
+                    violence, indecent depictions of minors, bestiality, necrophilia, etc., is strictly prohibited and
+                    will result in administrative action and when appropriate, notification to the proper authorities.
+                </p>
+
+                <p class="font-semibold text-xl">Limitation of liability for internal content:</p>
+                <p class="pb-4">
+                    The content of our website has been compiled with meticulous care and to the best of our knowledge.
+                    However, we cannot assume any liability for the up-to-dateness, completeness or accuracy of any of
+                    the pages.
+                </p>
+                <p class="pb-4">
+                    Pursuant to section 7, para. 1 of the TMG (Telemediengesetz – Tele Media Act by German law), we as
+                    service providers are liable for our own content on these pages in accordance with general laws.
+                    However, pursuant to sections 8 to 10 of the TMG, we as service providers are not under obligation
+                    to monitor external information provided or stored on our website. Once we have become aware of a
+                    specific infringement of the law, we will immediately remove the content in question. Any liability
+                    concerning this matter can only be assumed from the point in time at which the infringement becomes
+                    known to us.
+                </p>
+
+                <p class="font-semibold text-xl">Limitation of liability for external links:</p>
+                <p class="pb-4">
+                    Our website contains links to the websites of third parties (“external links”). As the content of
+                    these websites is not under our control, we cannot assume any liability for such external content.
+                    In all cases, the provider of information of the linked websites is liable for the content and
+                    accuracy of the information provided. At the point in time when the links were placed, no
+                    infringements of the law were recognisable to us. As soon as an infringement of the law becomes
+                    known to us, we will immediately remove the link in question.
+                </p>
+
+                <p class="font-semibold text-xl">Copyright:</p>
+                <p class="pb-4">
+                    The content and works published on this website are governed by the copyright laws of of the country
+                    this site is hosted from. Any duplication, processing, distribution or any form of utilisation
+                    beyond the scope of copyright law shall require the prior written consent of the author or authors
+                    in question.
+                </p>
+
+                <p class="font-semibold text-xl">Data protection:</p>
+                <p class="pb-4">
+                    A visit to our website can result in the storage on our server of information about the access
+                    (date, time, page accessed). This does not represent any analysis of personal data (e.g., name,
+                    address or email address). If personal data are collected, this only occurs – to the extent possible
+                    – with the prior consent of the user of the website. Any forwarding of the data to third parties
+                    without the express consent of the user shall not take place.
+                </p>
+                <p class="pb-4">
+                    We would like to expressly point out that the transmission of data via the Internet (e.g., by email)
+                    can offer security vulnerabilities. It is therefore impossible to safeguard the data completely
+                    against access by third parties. We cannot assume any liability for damages arising as a result of
+                    such security vulnerabilities.
+                </p>
+                <p class="pb-4">
+                    The use by third parties of all published contact details for the purpose of advertising is
+                    expressly excluded. We reserve the right to take legal steps in the case of the unsolicited sending
+                    of advertising information; e.g., by means of spam mail.
+                </p>
             </div>
         </div>
     </div>
