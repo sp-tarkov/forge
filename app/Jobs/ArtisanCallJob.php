6c4ba0152e4701531d8c65e3528e21c3b1f071f4
Refringe <me@refringe.com>
2025-05-31 16:16:13 -0400

    ArtisanCallJob For Queuing Artisan Jobs
    This allows us to queue artisan jobs from within a bus. Bus jobs must be able to be serialized, or they will fail silently. The anonymous functions we were using could not be serialized. This should resolve the Meilisearch server from falling out of sync again.

---
+++ app/Jobs/ArtisanCallJob.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
+
+/**
+ * @phpstan-type Arguments array<string, mixed>
+ */
+class ArtisanCallJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * @param  array<string, mixed>  $arguments
+     */
+    public function __construct(public string $command, public array $arguments = []) {}
+
+    public function handle(): void
+    {
+        Artisan::call($this->command, $this->arguments);
+    }
+}
