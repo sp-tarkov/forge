73c5d536b0644cf997a789955f7c6f5dbdd76a23
waffle.lord <waffle.lord@hotmail.com>
2025-06-19 14:56:35 -0400

    add addon and addon version migrations

--- database/migrations/2025_06_17_144026_create_mod_addons_table.php
+++ database/migrations/2025_06_17_144026_create_mod_addons_table.php
@@ -15,7 +15,40 @@ return new class extends Migration
     {
         Schema::create('mod_addons', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('owner_id')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete()
+                ->cascadeOnDelete();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('teaser');
+            $table->longText('description');
+            $table->string('thumbnail')->default('');
+            $table->foreignIdFor('licenses')
+                ->nullable()
+                ->default(null)
+                ->constrained('licenses')
+                ->nullOnDelete()
+                ->cascadeOnUpdate();
+            $table->unsignedBigInteger('downloads')->default(0);
+            $table->string('source_code_url');
+            $table->boolean('disabled')->default(false);
+            $table->boolean('featured')->default(false);
+            $table->boolean('contains_ai_content')->default(false);
+            $table->boolean('contains_ads')->default(false);
+            $table->timestamp('published_at')->nullable()->default(null);
             $table->timestamps();
+
+            $table->index('name');
+            $table->index(['slug']);
+            $table->index(['featured']);
+            $table->index('contains_ads');
+            $table->index('contains_ai_content');
+            $table->index('created_at');
+            $table->index('updated_at');
+            $table->index('published_at');
+            $table->index('disabled');
         });
     }
 
--- database/migrations/2025_06_17_144038_create_mod_addon_versions_table.php
+++ database/migrations/2025_06_17_144038_create_mod_addon_versions_table.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Models\ModAddon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,29 @@ return new class extends Migration
     {
         Schema::create('mod_addon_versions', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(ModAddon::class)
+                ->constrained('mod_addons')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->string('version');
+            $table->unsignedInteger('version_major')->default(0);
+            $table->unsignedInteger('version_minor')->default(0);
+            $table->unsignedInteger('version_patch')->default(0);
+            $table->string('version_pre_release')->default('');
+            $table->longText('description');
+            $table->string('link');
+            $table->string('spt_version_constraint');
+            $table->string('virus_total_link');
+            $table->unsignedBigInteger('downloads');
+            $table->boolean('disabled')->default(false);
+            $table->timestamp('published_at')->nullable()->default(null);
             $table->timestamps();
+
+            $table->index(['version']);
+            $table->index(['mod_addon_id']);
+            $table->index(['disabled']);
+            $table->index(['published_at']);
+            $table->index(['version_major', 'version_minor', 'version_patch', 'version_pre_release'], 'mod_addon_versions_version_components_index');
         });
     }
 
