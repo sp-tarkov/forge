56a90e81be5ab16ba8f77fa88e8832ce164ef551
Refringe <me@refringe.com>
2025-06-25 13:44:16 -0400

    Migrates comment form component into the listing component.

--- app/Livewire/Comment/Listing.php
+++ app/Livewire/Comment/Listing.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace App\Livewire\Comment;
 
+use App\Models\Comment;
 use App\Models\Mod;
 use Illuminate\Contracts\View\View;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Locked;
 use Livewire\Attributes\On;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -17,19 +20,75 @@ class Listing extends Component
     use WithPagination;
 
     /**
-     * The commentable. An instance of the model that can be commented on.
+     * The commentable model.
      *
      * @var Mod
      */
     #[Locked]
-    public Model $commentable;
+    public Mod $commentable; // Type-hinting to Mod for stronger type safety
+
+    /**
+     * The body of the comment.
+     *
+     * @var string
+     */
+    #[Validate('required')]
+    public $body = '';
+
+    /**
+     * A count of all comments on the commentable.
+     */
+    #[Computed]
+    public function commentCount(): int
+    {
+        return $this->commentable->comments()->count();
+    }
 
     /**
      * Executed when the component is first loaded.
      */
-    public function mount(Model $commentable): void
+    public function mount(Mod $commentable): void // Type-hinting to Mod for consistency
     {
         $this->commentable = $commentable;
+    }
+
+    /**
+     * Create a new comment.
+     */
+    public function create(): void
+    {
+        $this->authorize('create', Comment::class);
+
+        $this->validate();
+
+        $this->commentable->comments()->create([
+            'body' => $this->body,
+            'user_id' => Auth::id(),
+        ]);
+
+        $this->reset('body');
+
+        unset($this->commentCount); // Invalidate the computed property cache to reflect new comment
+
+        $this->dispatch('comment-saved')
+            ->self();
+    }
+
+    /**
+     * Update an existing comment.
+     */
+    public function update(Comment $comment): void
+    {
+        $this->authorize('update', $comment);
+
+        $this->validate();
+        // Using $this->body directly is also an option: ['body' => $this->body]
+        $comment->update(
+            $this->pull(['body'])
+        );
+
+        $this->dispatch('comment-saved')
+            ->self();
     }
 
     /**
@@ -46,7 +105,7 @@ class Listing extends Component
      */
     public function render(): View
     {
-        $rootComments = $this->commentable->rootComments;
+        $rootComments = $this->commentable->rootComments()->paginate(10);
 
         return view('livewire.comment.listing', ['rootComments' => $rootComments]);
     }
--- resources/views/livewire/comment/listing.blade.php
+++ resources/views/livewire/comment/listing.blade.php
@@ -1,9 +1,41 @@
 <div>
     @auth
-        <livewire:comment.form
-            wire:key="comment-form-{{ $commentable->id }}"
-            :commentable="$commentable"
-        />
+        <div class="p-6 mb-6 bg-white dark:bg-gray-950 rounded-xl shadow-md dark:shadow-gray-950 drop-shadow-2xl">
+            <div class="flex items-center justify-between mb-4">
+                <h2 class="text-xl font-bold text-white">
+                    {{ __('Discussion') }}
+                    <span class="font-normal text-slate-400">{{ '('.$this->commentCount.')' ?? '' }}</span>
+                </h2>
+                {{--
+                    TODO: Add subscribe button to get notified when a new comment is posted.
+                    <flux:button size="sm">{{ __('Subscribe') }}</flux:button>
+                --}}
+            </div>
+            <div class="flex items-start">
+                <div class="mr-3">
+                    <flux:avatar src="{{ auth()->user()->profile_photo_url }}" color="auto" color:seed="{{ auth()->user()->id }}" circle="circle"/>
+                </div>
+                <form class="flex-1" wire:submit="create">
+                    <flux:textarea
+                        name="body"
+                        wire:model="body"
+                        resize="vertical"
+                        placeholder="{{ __('Please ensure your comment does not break the community guidelines.') }}"
+                    />
+                    @error('body')
+                        <div class="text-red-500 text-xs my-1.5">{{ $message }}</div>
+                    @enderror
+                    <div class="flex items-center justify-between mt-2">
+                        <flux:button variant="primary" size="sm" class="text-black dark:text-white hover:bg-cyan-400 dark:hover:bg-cyan-600 bg-cyan-500 dark:bg-cyan-700" type="submit">
+                            {{ __('Post Comment') }}
+                        </flux:button>
+                        <div class="text-xs text-slate-400 text-right ml-2">
+                            {{ __('Basic Markdown formatting is supported.') }}
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     @endauth
 
     @foreach ($rootComments as $comment)
@@ -17,7 +49,10 @@
             @if ($comment->descendants->count() > 0)
                 <div class="transition-all duration-600 mt-4" x-show="showReplies{{ $comment->id }}" x-collapse>
                     @foreach ($comment->descendants as $descendant)
-                        <div class="p-6 mb-4 last:mb-0 bg-gray-50 dark:bg-gray-900 rounded-xl shadow-md dark:shadow-gray-950 drop-shadow-2xl filter-none transition-all duration-600">
+                        <div
+                            wire:key="comment-card-{{ $descendant->id }}"
+                            class="p-6 mb-4 last:mb-0 bg-gray-50 dark:bg-gray-900 rounded-xl shadow-md dark:shadow-gray-950 drop-shadow-2xl filter-none transition-all duration-600"
+                        >
                             <livewire:comment.card wire:key="comment-card-{{ $descendant->id }}" :comment="$descendant" />
                         </div>
                     @endforeach
