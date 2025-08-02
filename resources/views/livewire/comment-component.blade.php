f9d7745eff5cb3b2c041c6a0ddde9056c5a08264
Refringe <me@refringe.com>
2025-08-01 20:15:55 -0400

    fix: both download buttons from showing up in multiple breakpoints

--- resources/views/livewire/comment-component.blade.php
+++ resources/views/livewire/comment-component.blade.php
@@ -22,7 +22,12 @@
             </div>
             <div class="flex items-start">
                 <div class="mr-3">
-                    <flux:avatar src="{{ auth()->user()->profile_photo_url }}" color="auto" color:seed="{{ auth()->user()->id }}" circle="circle"/>
+                    <flux:avatar
+                        src="{{ auth()->user()->profile_photo_url }}"
+                        color="auto"
+                        color:seed="{{ auth()->user()->id }}"
+                        circle="circle"
+                    />
                 </div>
                 <div class="flex-1">
                     <x-comment.form
@@ -42,33 +47,54 @@
     @endif
 
     @foreach ($visibleRootComments as $comment)
-        <div wire:key="comment-container-{{ $comment->id }}-{{ $comment->updated_at->timestamp }}" class="relative mb-4 last:mb-0">
+        <div
+            wire:key="comment-container-{{ $comment->id }}-{{ $comment->updated_at->timestamp }}"
+            class="relative mb-4 last:mb-0"
+        >
             <livewire:ribbon.comment
                 wire:key="ribbon-comment-{{ $comment->id }}-{{ $comment->updated_at->timestamp }}"
                 :comment-id="$comment->id"
                 :spam-status="$comment->spam_status->value"
                 :can-see-ribbon="auth()->user()?->can('seeRibbon', $comment) ?? false"
             />
-            <div wire:key="comment-{{ $comment->id }}-{{ $comment->updated_at->timestamp }}"
-                 x-data="{ showReplies: $persist({{ ($showReplies[$comment->id] ?? true) ? 'true' : 'false' }}).as('comment-replies-{{ $comment->id }}') }"
-                 class="p-6 bg-white dark:bg-gray-950 rounded-xl shadow-md dark:shadow-gray-950 drop-shadow-2xl filter-none transition-all duration-600">
-
-                <x-comment.display :comment="$comment" :manager="$this" :commentable="$commentable" />
+            <div
+                wire:key="comment-{{ $comment->id }}-{{ $comment->updated_at->timestamp }}"
+                x-data="{ showReplies: $persist({{ ($showReplies[$comment->id] ?? true) ? 'true' : 'false' }}).as('comment-replies-{{ $comment->id }}') }"
+                class="p-6 bg-white dark:bg-gray-950 rounded-xl shadow-md dark:shadow-gray-950 drop-shadow-2xl filter-none transition-all duration-600"
+            >
+                <x-comment.display
+                    :comment="$comment"
+                    :manager="$this"
+                    :commentable="$commentable"
+                />
 
                 @if ($comment->descendants->count() > 0)
-                    <div x-show="showReplies"
-                         x-collapse
-                         class="mt-4 space-y-4">
+                    <div
+                        x-show="showReplies"
+                        x-collapse
+                        class="mt-4 space-y-4"
+                    >
                         @foreach ($comment->descendants as $descendant)
-                            <div wire:key="descendant-container-{{ $descendant->id }}-{{ $descendant->updated_at->timestamp }}" class="relative">
+                            <div
+                                wire:key="descendant-container-{{ $descendant->id }}-{{ $descendant->updated_at->timestamp }}"
+                                class="relative"
+                            >
                                 <livewire:ribbon.comment
                                     wire:key="ribbon-descendant-{{ $descendant->id }}-{{ $descendant->updated_at->timestamp }}"
                                     :comment-id="$descendant->id"
                                     :spam-status="$descendant->spam_status->value"
                                     :can-see-ribbon="auth()->user()?->can('seeRibbon', $descendant) ?? false"
                                 />
-                                <div wire:key="descendant-{{ $descendant->id }}-{{ $descendant->updated_at->timestamp }}" class="p-6 bg-gray-50 dark:bg-gray-900 rounded-xl shadow-md dark:shadow-gray-950 drop-shadow-2xl filter-none transition-all duration-600">
-                                    <x-comment.display :comment="$descendant" :manager="$this" :is-reply="true" :commentable="$commentable" />
+                                <div
+                                    wire:key="descendant-{{ $descendant->id }}-{{ $descendant->updated_at->timestamp }}"
+                                    class="p-6 bg-gray-50 dark:bg-gray-900 rounded-xl shadow-md dark:shadow-gray-950 drop-shadow-2xl filter-none transition-all duration-600"
+                                >
+                                    <x-comment.display
+                                        :comment="$descendant"
+                                        :manager="$this"
+                                        :is-reply="true"
+                                        :commentable="$commentable"
+                                    />
                                 </div>
                             </div>
                         @endforeach
