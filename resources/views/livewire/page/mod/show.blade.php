c00f16db01af30fdb119b2c9e55b19d4eb7200e2
Refringe <me@refringe.com>
2025-09-24 14:39:15 -0400

    feat: add profile binding warning for applicable mod categories
    Adds a conditional warning callout for mod categories that may bind to user profiles. Includes link to documentation on the Wiki.
    
    Resolves #283

--- resources/views/livewire/page/mod/show.blade.php
+++ resources/views/livewire/page/mod/show.blade.php
@@ -236,6 +236,16 @@
                 />
             @endif
 
+            {{-- Profile Binding Notice --}}
+            @if ($requiresProfileBindingNotice)
+                <flux:callout icon="information-circle" color="orange" inline="inline">
+                    <flux:callout.heading>Possibly Profile Binding</flux:callout.heading>
+                    <flux:callout.text>
+                        This mod <em>may</em> make permanent changes to your profile, and <em>may</em> not be removable without starting a new profile. Ensure profile backups exist before using it. <a href="https://wiki.sp-tarkov.com/Installing_Mods#profiles" target="_blank" class="underline text-orange-700 hover:text-orange-800 dark:text-orange-300 dark:hover:text-orange-200">Read more.</a>
+                    </flux:callout.text>
+                </flux:callout>
+            @endif
+
             {{-- Additional Mod Details --}}
             <div class="p-4 sm:p-6 bg-white dark:bg-gray-950 rounded-xl shadow-md dark:shadow-gray-950 drop-shadow-2xl">
                 <h2 class="text-2xl font-bold text-gray-900 dark:text-gray-100">{{ __('Details') }}</h2>
