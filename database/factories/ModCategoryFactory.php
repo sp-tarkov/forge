fd75bf6f91cb55c0a5d5fda67831346210803f8c
Refringe <me@refringe.com>
2025-09-27 20:17:57 -0400

    fix: add slug generation to ModCategoryFactory

--- database/factories/ModCategoryFactory.php
+++ database/factories/ModCategoryFactory.php
@@ -6,6 +6,7 @@ namespace Database\Factories;
 
 use App\Models\ModCategory;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends Factory<ModCategory>
@@ -19,8 +20,11 @@ class ModCategoryFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->unique()->words(rand(2, 4), true);
+
         return [
-            'title' => fake()->unique()->words(rand(2, 4), true),
+            'title' => $title,
+            'slug' => Str::slug($title),
             'description' => fake()->sentence(),
         ];
     }
