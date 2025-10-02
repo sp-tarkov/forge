048babd92675b679a54047052cb459c7cfe2e036
Refringe <me@refringe.com>
2025-10-01 21:11:54 -0400

    feat: add high-DPI logo support with responsive image variants

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,4 +1,9 @@
 <div class="flex flex-nowrap justify-start items-center gap-1">
-    <img src="{{ Vite::asset('resources/images/spt-small.png') }}" alt="The Single Player Tarkov Logo" style="width:32px;height:16px;" class="pt-0.5" />
-    <h1 class="text-lg font-extrabold text-black dark:text-white">Forge</h1>
+    <img
+        src="{{ Vite::asset('resources/images/spt-small.png') }}"
+        srcset="{{ Vite::asset('resources/images/spt-small.png') }} 1x, {{ Vite::asset('resources/images/spt-small@2x.png') }} 2x, {{ Vite::asset('resources/images/spt-small@3x.png') }} 3x, {{ Vite::asset('resources/images/spt-small@4x.png') }} 4x"
+        alt="Single Player Tarkov Logo"
+        class="w-[32px] h-[17px] pt-[1px]"
+    />
+    <h1 class="text-[1.15rem] font-extrabold text-black dark:text-white">Forge</h1>
 </div>
