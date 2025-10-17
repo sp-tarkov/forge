a78632cf35614c48e98b651d468624ba6dffcf50
Refringe <me@refringe.com>
2025-10-17 04:08:05 +0000

    style: automatic fixes

--- app/Observers/CommentObserver.php
+++ app/Observers/CommentObserver.php
@@ -22,8 +22,8 @@ class CommentObserver
         $maxLength = config('comments.validation.max_length', 10000);
 
         throw_if(mb_strlen($comment->body) > $maxLength,
-        InvalidArgumentException::class,
-        sprintf('Comment body cannot exceed %d characters.', $maxLength));
+            InvalidArgumentException::class,
+            sprintf('Comment body cannot exceed %d characters.', $maxLength));
     }
 
     /**
