3dbcf237de3245c6c1894236618652712a189a77
Refringe <me@refringe.com>
2025-10-20 00:08:51 -0400

    refactor: improve comment form button layout

--- resources/views/components/comment/form.blade.php
+++ resources/views/components/comment/form.blade.php
@@ -13,23 +13,35 @@
         <div class="text-red-500 text-xs my-1.5">{{ $message }}</div>
     @enderror
     <div class="flex items-center justify-between mt-2">
-        <flux:button
-            variant="primary"
-            size="sm"
-            class="text-black dark:text-white hover:bg-cyan-400 dark:hover:bg-cyan-600 bg-cyan-500 dark:bg-cyan-700"
-            type="submit">
-            {{ $submitText }}
-        </flux:button>
         @if($cancelAction)
-            <flux:button
-                type="button"
-                wire:click="{{ $cancelAction }}"
-                data-test="cancel-{{ $dataTest ?? str_replace('.', '-', $formKey) }}"
-                variant="danger"
-                size="sm">
-                {{ __('Cancel') }}
-            </flux:button>
+            <div class="flex items-center gap-2">
+                <flux:button
+                    variant="primary"
+                    size="sm"
+                    class="text-black dark:text-white hover:bg-cyan-400 dark:hover:bg-cyan-600 bg-cyan-500 dark:bg-cyan-700"
+                    type="submit">
+                    {{ $submitText }}
+                </flux:button>
+                <flux:button
+                    type="button"
+                    wire:click="{{ $cancelAction }}"
+                    data-test="cancel-{{ $dataTest ?? str_replace('.', '-', $formKey) }}"
+                    variant="ghost"
+                    size="sm">
+                    {{ __('Cancel') }}
+                </flux:button>
+            </div>
+            <div class="text-xs text-slate-400 text-right ml-2">
+                {{ __('Basic Markdown formatting is supported.') }}
+            </div>
         @else
+            <flux:button
+                variant="primary"
+                size="sm"
+                class="text-black dark:text-white hover:bg-cyan-400 dark:hover:bg-cyan-600 bg-cyan-500 dark:bg-cyan-700"
+                type="submit">
+                {{ $submitText }}
+            </flux:button>
             <div class="text-xs text-slate-400 text-right ml-2">
                 {{ __('Basic Markdown formatting is supported.') }}
             </div>
