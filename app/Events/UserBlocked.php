148f670cee3944fa6e8775dc73b25539e7e66c6c
Refringe <me@refringe.com>
2026-03-04 12:08:59 -0500

    fix: use ShouldBroadcastNow for UserBlocked and UserUnblocked events

--- app/Events/UserBlocked.php
+++ app/Events/UserBlocked.php
@@ -8,11 +8,11 @@ use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class UserBlocked implements ShouldBroadcast
+class UserBlocked implements ShouldBroadcastNow
 {
     use Dispatchable;
     use InteractsWithSockets;
--- app/Events/UserUnblocked.php
+++ app/Events/UserUnblocked.php
@@ -8,11 +8,11 @@ use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class UserUnblocked implements ShouldBroadcast
+class UserUnblocked implements ShouldBroadcastNow
 {
     use Dispatchable;
     use InteractsWithSockets;
