64ea0a888533900d1a4ffab330a6bfa03e10223c
Refringe <me@refringe.com>
2026-03-04 12:18:57 -0500

    revert: restore ShouldBroadcast for UserBlocked and UserUnblocked events

--- app/Events/UserUnblocked.php
+++ app/Events/UserUnblocked.php
@@ -8,11 +8,11 @@ use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class UserUnblocked implements ShouldBroadcastNow
+class UserUnblocked implements ShouldBroadcast
 {
     use Dispatchable;
     use InteractsWithSockets;
