92323944eee3cc31875b7b3b3363703a8a928922
Refringe <me@refringe.com>
2026-04-13 22:50:59 -0400

    fix: restrict external-facing scheduled tasks to production environment

--- routes/console.php
+++ routes/console.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('horizon:snapshot')->everyFiveMinutes()->onOneServer();
 Schedule::command(CleanupOldNotificationLogs::class)->daily()->onOneServer();
 
-Schedule::command(UpdateGeoLiteDatabase::class)->daily()->at('02:00')->onOneServer()->runInBackground();
-Schedule::job(new UpdateDisposableEmailBlocklist)->daily()->at('04:00')->onOneServer();
+Schedule::command(UpdateGeoLiteDatabase::class)->daily()->at('02:00')->onOneServer()->runInBackground()->environments('production');
+Schedule::job(new UpdateDisposableEmailBlocklist)->daily()->at('04:00')->onOneServer()->environments('production');
 
-Schedule::job(new SendDiscordNotifications)->everyMinute()->onOneServer()->withoutOverlapping();
-Schedule::job(new ProcessPinnedModVersionPublishDates)->everyMinute()->onOneServer()->withoutOverlapping();
+Schedule::job(new SendDiscordNotifications)->everyMinute()->onOneServer()->withoutOverlapping()->environments('production');
+Schedule::job(new ProcessPinnedModVersionPublishDates)->everyMinute()->onOneServer()->withoutOverlapping()->environments('production');
 
 if (config('app.forge_heartbeat_url')) {
     Schedule::command(ForgeHeartbeat::class)->everyMinute()->onOneServer()->withoutOverlapping();
