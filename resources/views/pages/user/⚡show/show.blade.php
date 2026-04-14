5baebb26de5fdb19da158c625205a0092e3e5594
Refringe <me@refringe.com>
2026-04-13 22:11:32 -0400

    fix: use sanitized about_html accessor instead of raw Str::markdown on user profile

--- resources/views/pages/user/⚡show/show.blade.php
+++ resources/views/pages/user/⚡show/show.blade.php
@@ -170,7 +170,7 @@
                 @if ($user->about)
                     <div
                         class="p-4 sm:p-6 bg-white dark:bg-gray-950 rounded-xl shadow-md dark:shadow-gray-950 text-gray-800 dark:text-gray-200 drop-shadow-2xl">
-                        {!! Str::markdown($user->about) !!}
+                        {!! $user->about_html !!}
                     </div>
                 @endif
 
