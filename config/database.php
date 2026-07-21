2f7a6731cd3c1f7808eaa85019d9528805521a04
Refringe <me@refringe.com>
2026-07-21 09:54:45 -0400

    fix: reverts database env config changes

--- config/database.php
+++ config/database.php
@@ -107,18 +107,19 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_PG_URL'),
-            'host' => env('DB_PG_HOST', '127.0.0.1'),
-            'port' => env('DB_PG_PORT', '5432'),
-            'database' => env('DB_PG_DATABASE', 'laravel'),
-            'username' => env('DB_PG_USERNAME', 'root'),
-            'password' => env('DB_PG_PASSWORD', ''),
-            'charset' => env('DB_PG_CHARSET', 'utf8'),
-            'timezone' => env('DB_PG_TIMEZONE', 'UTC'),
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'timezone' => env('DB_TIMEZONE', 'UTC'),
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslrootcert' => env('DB_SSLROOTCERT'),
         ],
 
         'sqlsrv' => [
