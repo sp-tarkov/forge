1769abee777a003ec4d1e65f180ee049e4cdc589
Refringe <me@refringe.com>
2026-07-27 19:44:29 -0400

    fix: define static deprecations log channel

--- config/logging.php
+++ config/logging.php
@@ -120,6 +120,14 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'deprecations' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/deprecations.log'),
+            'level' => 'warning',
+            'days' => 7,
+            'replace_placeholders' => true,
+        ],
+
         'null' => [
             'driver' => 'monolog',
             'handler' => NullHandler::class,
